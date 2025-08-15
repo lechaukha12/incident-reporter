c6cfacbdff611183a765db987747c6d0ccd72a76
Le Chau Kha <lechaukha12@Les-MacBook-Pro.local>
2025-08-15 09:35:47 +0700

    update

--- frontend/src/app/message-config.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NZ_MESSAGE_CONFIG } from 'ng-zorro-antd/message';
-
-@NgModule({
-  providers: [
-    { provide: NZ_MESSAGE_CONFIG, useValue: { duration: 5000 } } // 5000ms = 5 giây
-  ]
-})
-export class MessageConfigModule { }
--- frontend/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: window.location.hostname === 'localhost' 
-    ? 'http://localhost:8080/api' 
-    : window.location.hostname === 'tunning-ops-sit.namabank.com.vn' 
-      ? '/api' 
-      : '/api'
-};
